c1b7a16662b99812cd9751b717d5003d10c67a07
Abdoul Aziz Yatagan <124500030+ABDOUSALAM1942@users.noreply.github.com>
2023-10-10 23:48:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,29 +5,17 @@ hrElement = document.createElement('hr');
 body.appendChild(hrElement);
 nav.insertAdjacentElement('afterend', hrElement);
 
-// Sélectionne l'élément HTML ayant la classe "chrono"
 const chronoElement = document.querySelector('.chrono');
-
-// Sélectionne l'élément HTML ayant l'ID "returnTimeText"
 const returnTimeTextElement = document.getElementById('returnTimeText');
-
 // Initialise la variable pour le temps restant à 20 secondes
 let remainingTime = 20;
-
-// Variable pour stocker l'ID de l'intervalle de compte à rebours
-let countDown;
-
-// Affiche le temps initial dans l'élément chrono
-displayTime(remainingTime);
-
-// Sélectionne tous les liens de la classe ".navbar a"
+let countDown;// Variable pour stocker l'ID de l'intervalle de compte à rebours
+displayTime(remainingTime);// Affiche le temps initial dans l'élément chrono
 const navLinks = document.querySelectorAll('.navbar a');
-
 // Ajoute un écouteur d'événements à chaque lien de navigation
 navLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
+    link.addEventListener('click', function (event) {
         event.preventDefault();
-
         // Map associant les ID de liens à des temps en secondes
         const timeMap = {
             nl2: 300,
@@ -38,29 +26,18 @@ navLinks.forEach(link => {
 
         // Obtient le temps à partir de la map ou utilise 20 secondes par défaut
         remainingTime = timeMap[link.id] || 20;
-
-        // Arrête le compte à rebours actuel
-        clearInterval(countDown);
-
-        // Démarre un nouveau compte à rebours
-        startCountDown();
-
+        clearInterval(countDown);        // Arrête le compte à rebours actuel
+        startCountDown();        // Démarre un nouveau compte à rebours
         // Calcule l'heure de retour
         const currentTime = new Date();
         const returnTime = new Date(currentTime.getTime() + remainingTime * 1000);
-
         // Obtient les heures et les minutes pour l'heure de retour
         const returnHours = returnTime.getHours();
         const returnMinutes = returnTime.getMinutes();
-
-        // Construit le texte d'heure de retour
         const returnTimeText = `Be back at ${returnHours < 10 ? '0' : ''}${returnHours}:${returnMinutes < 10 ? '0' : ''}${returnMinutes}`;
-
         // Met à jour le texte d'heure de retour
         returnTimeTextElement.textContent = returnTimeText;
-
-        // Affiche le compteur après le clic sur un lien
-        chronoElement.style.display = 'block';
+        chronoElement.style.display = 'block';        // Affiche le compteur après le clic sur un lien
     });
 });
 
@@ -93,21 +70,18 @@ function displayTime(seconds) {
 function endTime() {
     const currentTime = new Date();
     const returnTime = new Date(currentTime.getTime() + remainingTime * 1000);
-
     const returnHours = returnTime.getHours();
     const returnMinutes = returnTime.getMinutes();
-
     const returnTimeText = `Be back at ${returnHours < 10 ? '0' : ''}${returnHours}:${returnMinutes < 10 ? '0' : ''}${returnMinutes}`;
 
     // Met à jour le texte d'heure de retour
     returnTimeTextElement.textContent = returnTimeText;
 }
 
-// Sélectionne l'élément HTML pour la saisie personnalisée du temps
 const customTimeInput = document.getElementById('typeText');
 
 // Ajoute un écouteur d'événements pour la touche "Enter" dans la saisie personnalisée
-customTimeInput.addEventListener('keyup', function(event) {
+customTimeInput.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
         // Convertit l'entrée en minutes personnalisées en secondes
         const customMinutes = parseInt(customTimeInput.value);
@@ -129,3 +103,6 @@ customTimeInput.addEventListener('keyup', function(event) {
 
 
 
+// const birthday = new Date(1994, 9, 29);
+// const copy = new Date();
+// copy.getTime(birthday.getTime());
